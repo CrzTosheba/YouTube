1e431a495fe15d6b03e7b4b5f70717aa33a20a60
CrzTosheba <fugu@list.ru>
2022-06-09 21:34:11 +0300

    Добавьте файлы проекта.

---
+++ YouTubeDownloader/Command/SimpleCommands/GetManifestCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace YouTubeDownloader.Command.SimpleCommands;
+
+using System.Threading.Tasks;
+using YoutubeExplode.Videos.Streams;
+
+public class GetManifestCommand : ICommand
+{
+    private readonly CommandContext _commandContext;
+
+    public GetManifestCommand(CommandContext commandContext)
+    {
+        _commandContext = commandContext;
+    }
+
+    public async Task Execute()
+    {
+        var client = _commandContext.Client;
+        var video = _commandContext.VideoId;
+        var manifest = await client.Videos.Streams.GetManifestAsync(video);
+        _commandContext.Manifest = manifest; 
+        var videoInfo = manifest.GetMuxedStreams().GetWithHighestVideoQuality();
+        _commandContext.Info = videoInfo;
+    }
+}
---
+++ YouTubeDownloader/Command/SimpleCommands/WriteToConsoleCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace YouTubeDownloader.Command.SimpleCommands;
+
+using System;
+using System.Threading.Tasks;
+
+public class WriteToConsoleCommand : ICommand
+{
+    private readonly CommandContext _context;
+
+    public WriteToConsoleCommand(CommandContext context)
+    {
+        _context = context;
+    }
+
+    public Task Execute()
+    {
+        Console.WriteLine($"VideoCodec: {_context.Info.VideoCodec}");
+        return Task.CompletedTask;
+    }
+}
